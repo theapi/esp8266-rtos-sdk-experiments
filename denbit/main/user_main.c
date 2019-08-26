f89d5f0dbcd8fb254668072fd5e049d9edfad52b
Peter Clarke <petermclarke@gmail.com>
2019-08-26 12:37:27 +0100

    Blink with a task

--- denbit/main/user_main.c
+++ denbit/main/user_main.c
@@ -9,19 +9,34 @@
 
 #include "denbit.h"
 
-void app_main(void)
+void blinkTask(void *pvParameters)
 {
-    denbit_config();
-
     int cnt = 0;
-    while (1) {
+    while(1) {
         vTaskDelay(1000 / portTICK_RATE_MS);
         gpio_set_level(DENBIT_GREEN, cnt % 2);
         gpio_set_level(DENBIT_RGB_GREEN, cnt % 2);
         cnt++;
         gpio_set_level(DENBIT_RED, cnt % 2);
         gpio_set_level(DENBIT_RGB_RED, cnt % 2);
-        //printf("cnt:%i\n", cnt);
     }
+}
+
+void app_main(void)
+{
+    denbit_config();
+
+    xTaskCreate(blinkTask, "blinkTask", 180, NULL, 2, NULL);
+
+    // int cnt = 0;
+    // while (1) {
+    //     vTaskDelay(1000 / portTICK_RATE_MS);
+    //     gpio_set_level(DENBIT_GREEN, cnt % 2);
+    //     gpio_set_level(DENBIT_RGB_GREEN, cnt % 2);
+    //     cnt++;
+    //     gpio_set_level(DENBIT_RED, cnt % 2);
+    //     gpio_set_level(DENBIT_RGB_RED, cnt % 2);
+    //     //printf("cnt:%i\n", cnt);
+    // }
 }
 
