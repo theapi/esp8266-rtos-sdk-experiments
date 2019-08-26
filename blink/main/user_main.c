062cedf79dedaa8a9105654f7a98c8c39b2df34e
Peter Clarke <petermclarke@gmail.com>
2019-08-26 12:32:29 +0100

    Blink with a task

--- blink/main/user_main.c
+++ blink/main/user_main.c
@@ -1,5 +1,4 @@
 
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -9,6 +8,35 @@
 
 #define GPIO_OUTPUT_IO_0    2
 #define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_0)
+
+    /* Dimensions the buffer that the task being created will use as its stack.
+    NOTE:  This is the number of words the stack will hold, not the number of
+    bytes.  For example, if each stack item is 32-bits, and this is set to 100,
+    then 400 bytes (100 * 32-bits) will be allocated. */
+    #define STACK_SIZE 200
+
+    /* Structure that will hold the TCB of the task being created. */
+    StaticTask_t xTaskBuffer;
+
+    /* Buffer that the task being created will use as its stack.  Note this is
+    an array of StackType_t variables.  The size of StackType_t is dependent on
+    the RTOS port. */
+    StackType_t xStack[ STACK_SIZE ];
+
+
+void blinkTask(void *pvParameters)
+{
+    int cnt = 0;
+    // UBaseType_t uxHighWaterMark;
+    // uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
+    while(1) {
+        vTaskDelay(500 / portTICK_RATE_MS);
+        gpio_set_level(GPIO_OUTPUT_IO_0, ++cnt % 2);
+        // uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
+        // printf("cnt:%i\n", cnt);
+        // printf("uxHighWaterMark:%lu\n", uxHighWaterMark);
+    }
+}
 
 
 void app_main(void)
@@ -27,13 +55,15 @@ void app_main(void)
     //configure GPIO with the given settings
     gpio_config(&io_conf);
 
+    xTaskCreate(blinkTask, "blinkTask", 180, NULL, 2, NULL);
 
-    int cnt = 0;
+    // int cnt = 0;
+    // while (1) {
+    //     vTaskDelay(500 / portTICK_RATE_MS);
+    //     gpio_set_level(GPIO_OUTPUT_IO_0, ++cnt % 2);
+    //     printf("cnt:%i\n", cnt);
+    // }
 
-    while (1) {
-        vTaskDelay(500 / portTICK_RATE_MS);
-        gpio_set_level(GPIO_OUTPUT_IO_0, ++cnt % 2);
-        printf("cnt:%i\n", cnt);
-    }
+
 }
 
